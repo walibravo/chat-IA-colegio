beb79ddc938d50857923a0b0fcdc0b11ecd58dc9
walibravo <malasbro11@gmail.com>
2024-03-16 22:46:05 -0500

    inicializando proyecto

---
+++ src/Components/Header.tsx
@@ -0,0 +1,15 @@
+export function Header(){
+    return(
+        <>
+        <nav>
+            <div className="">Logo</div>
+            <ul className="flex flex-row bg-white">
+                <li>......</li>
+                <li>......</li>
+                <li>,.....</li>
+            </ul>
+        </nav>
+        
+        </>
+    )
+}
---
+++ src/Components/Informacion.tsx
@@ -0,0 +1,7 @@
+export function Informacion(){
+    return(
+        <>
+        
+        </>
+    )
+}
